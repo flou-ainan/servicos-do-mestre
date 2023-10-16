c5721f9d901ec9cc492dc4b559a19f173fb1aed9
flouainan <flouainan@gmail.com>
2023-10-16 14:50:50 -0300

    vercel-auoto-deploy-test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <h1>Meu app <a href='meu-app'>{'-->'}</a></h1>
+      <h1>Meu app {'  '}<a href='meu-app'>{'-->'}</a></h1>
     </main>
   )
 }
--- src/pages/meu-app.js
+++ src/pages/meu-app.js
@@ -2,7 +2,7 @@ export default function meuApp(){
     return(
         <>
             <h1>Bem Vindo(a) ao meu App</h1><br/>
-            <a href="./">{'<--'}</a>
+            <a href="./">{'  <--|'}</a>
         </>
     )
 }
