a9f422defa954579252d7f3ea9ee723996af6901
flouainan <flouainan@gmail.com>
2023-10-17 03:26:08 -0300

    first part of typewriter reimplementation

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,28 +1,61 @@
+import { useEffect, useState } from "react";
 import Nav from "./Nav";
 
+
 export default function Header() {
+    const titleText = "O Melhor do Digital"
+    const firstSubText = "Feito sob medida para você"
+    const secondSubText = "do seu jeito" 
+
+    const [content, setContent] = useState({
+        title: {
+            text: titleText,
+            barState: "hidden"
+        },
+        firstSub: {
+            text: firstSubText,
+            barState: "hidden"
+        },
+        secondSub: {
+            text: secondSubText,
+            barState: "hidden"
+        },
+    })
+
+ 
+    useEffect(()=>{
+       typeWrite(content, setContent, ['title', 'firstSub', 'secondSub'])
+    },[])
+    
+
+
+
+
     return (
         <header className="hero">
             <Nav />
             <div className="hero-content">
-                <Herofirst />
+                <Herofirst content={content} />
                 <Herosecond />
             </div >
         </header>
     )
 }
-
-
-const Herofirst = () =>
-    <div className="hero-first shadow-text">
-        <h1>
-            <span id="header-text" className="shadow-text">O Melhor do Digital</span><span id="header-text-bar" className="blinker bar">|</span>
-        </h1>
-        <p id="header-subtitle">
-            <span id="subtitle-text-01">Feito sob medida para você</span><span id="subtitle-text-01-bar" className="blinker bar">|</span>
-            <br /><br /><b><span id="subtitle-text-02" className="bold">do seu jeito</span></b><span id="subtitle-text-02-bar" className="blinker bar">|</span>
-        </p>
-    </div>
+const Herofirst = (props) =>{
+    const {title, firstSub, secondSub} = props.content
+    return(
+        <div className="hero-first shadow-text">
+            <h1>
+                <span id="title-text" className="shadow-text">{title.text}</span><span className={title.barState} style={{opacity: '1'}}>|</span>
+            </h1>
+            <p id="header-subtitle">
+                <span>{firstSub.text}</span><span className={firstSub.barState}>|</span>
+                <br />
+                <br />
+                <b><span className="bold">{secondSub.text }</span></b><span className={secondSub.barState}>|</span>
+            </p>
+        </div>
+    )}
 
 const Herosecond = () =>
     <div className="hero-second">
@@ -30,3 +63,99 @@ const Herosecond = () =>
         <br />
         <a href="#servicos" className="typer-cta">CONTRATAR AGORA</a>
     </div>
+
+
+
+
+function typeWrite(content, setContent, elementsIds){ //Recieve state object getter end setter and an array of the elements ids in order
+    let i = 0;
+    let counter;
+    let textsArray = []
+    let startDelay = 950 // starting type delay in milliseconds
+    const typingSpeed = 50 // interval between types in milliseconds
+    const wordDelay = 300 // waiting time between words in milliseconds
+   
+
+    
+    //save all contents data before erasing
+    textsArray = elementsIds.map((elementID) => {
+       return content[elementID].text
+    })
+
+
+    
+    // erase all content data
+    
+    setContent(prevContent => {
+        let newContent = { ...prevContent }
+        for (let index = 0; index < elementsIds.length; index++) {
+            let element = newContent[elementsIds[index]]
+            element.text = ""
+            newContent[elementsIds[index]] = element
+        }
+        return newContent
+
+    })
+
+    const setBarState = (elementId, stateClass) => {
+        setContent(prevContent =>{
+            let newContent = {...prevContent}
+            let element = newContent[elementId]
+            element.barState=stateClass
+            newContent[elementId] = element
+            return newContent
+        })
+    }
+
+    console.log(content)
+    setBarState('firstSub', "blinker")
+    
+    // recieve an array of DOM element IDs that have innerHTML atribute and typewrite them.
+    let typeNow = (elementsIds) => {
+       let element = $(elementsIds[i])
+       let barElement = $(`${elementsIds[i]}-bar`)
+       let word = contentsArray[i].split("")
+       element.innerHTML = ""
+ 
+       
+       var loopTyping = () => {
+          barElement.className = "typing"
+          if (word.length > 0) {
+             element.innerHTML += word.shift();
+          } else {
+            
+             counter = setTimeout(() => {
+                barElement.className = "blinker"
+                counter = setTimeout(()=>{
+                   barElement.className = "hidden"
+                   // Time to loop again hahaha
+                   i++
+                   if (elementsIds.length > i){
+                      typeNow(elementsIds)       //TYPING LOOP ENDING POINT
+                   }else{
+                     
+                      i=0
+                      contentsArray = []
+ 
+                      counter = setTimeout(() => {typeWrite(elementsIds)},7000)
+                      return  // TYPENOW ENDING POINT
+                   }
+             }, wordDelay)
+             }, 200)
+             return false;
+          };
+          counter = setTimeout(loopTyping, typingSpeed);
+       };
+ 
+       // Typing Loop first call
+       barElement.className = "typing"
+       counter = setTimeout(loopTyping, wordDelay)
+    };
+    
+    // typeNow first call
+    //$(`${elementsIds[0]}-bar`).className = "blinker"
+   // counter = setTimeout(() => typeNow(elementsIds), startDelay)
+ }
+ 
+// typeWrite(content, setContent, ['title', 'firstSub', 'seccondSub'])
+ 
