1d1a376c79a705183ff0d58604b4f42f02adb4c8
flouainan <flouainan@gmail.com>
2023-10-18 11:59:48 -0300

    google verification meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Dosis:wght@200;300;400;500;600;700&family=Mohave&family=Playfair+Display:wght@400;500;700&family=Quicksand:wght@300;400;500&family=Rubik:wght@300;400&family=Young+Serif&display=swap" rel="stylesheet" />
+      <meta name="google-site-verification" content="oR1WRvSgY82ve5JJq7jY4NfLwXQq9f0frPMs7IPc5aw" />
       </Head>
       <body>
         <Main />
