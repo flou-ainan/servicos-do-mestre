cfd727863273e7ffff3e3043a24c3154c39a474e
flouainan <flouainan@gmail.com>
2023-10-18 12:19:09 -0300

    Next Image component implemented

--- src/pages/components/Servicos.js
+++ src/pages/components/Servicos.js
@@ -1,61 +1,55 @@
 //         <div class="services-viewport">
 //       <h2 class="services-header">Serviços</h2>
 //       <section class="services" id="services">
-import { useId } from "react"
+
+import Image from "next/image"
+
 const services = [
     {
         label: "Arte Para Redes",
         imgName: "artes-para-redes.png",
         price: "19,00",
-        id: Math.random
         
     },
     {
         label: "Seu Comércio no Digital",
         imgName: "online-shop.png",
         price: "799,00",
-        id: Math.random
         
     },
     {
         label: "Website",
         imgName: "website.png",
         price: "399,00",
-        id: Math.random
         
     },
     {
         label: "Landing Page",
         imgName: "website-estatico.png",
         price: "299,00",
-        id: Math.random
         
     },
     {
         label: "Design de Logomarca",
         imgName: "logos-04-wshadow.jpg",
         price: "129,00",
-        id: Math.random
         
     },
     {
         label: "Edição de Vídeo",
         imgName: "edicao-de-video3.jpg",
         price: "59,00",
-        id: Math.random
         
     },
     {
         label: "Design Cartão de Visita",
         imgName: "cartao-de-visita.png",
         price: "139,00",
-        id: Math.random
     },
     {
         label: "Convite Animado Digital",
         imgName: "convite-animado.jpg",
         price: "29,00",
-        id: Math.random
     }
 ]
 
@@ -87,7 +81,12 @@ function ServiceCard(props) {
                 <span className="label-text">{label}</span>
             </div>
             <div className="service-card-section card-img-holder">
-                <img src={`/images/${imgName}`} />
+                <Image
+                    src={`/images/${imgName}`}
+                    width={250}
+                    height={250}
+                    alt={label}
+                />
             </div>
             <div className="card-footer">
                 <span className="left">A partir de R${price}</span>
